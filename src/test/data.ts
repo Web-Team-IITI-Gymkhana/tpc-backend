8f4536ba9ccac501c8563582b01a4c4743f5d946
Akash Gupta <agbichpuriya@gmail.com>
2023-12-27 22:10:49 +0530

    update data

--- src/test/data.ts
+++ src/test/data.ts
@@ -1,7 +1,10 @@
-import { YEARS } from "src/constants";
-import { Role, SeasonType } from "src/db/enums";
+import { Category, Gender, Role, SeasonType, TpcMemberRole } from "src/db/enums";
 import { Company } from "src/entities/Company";
+import { Faculty } from "src/entities/Faculty";
+import { Recruiter } from "src/entities/Recruiter";
 import { Season } from "src/entities/Season";
+import { Student } from "src/entities/Student";
+import { TpcMember } from "src/entities/TpcMember";
 import { User } from "src/entities/User";
 
 export const SEASON1: Season = {
@@ -48,12 +51,27 @@ export const USER2: User = {
   role: Role.STUDENT,
 };
 
+export const STUDENT1: Student = {
+  id: "student1",
+  userId: USER2.id,
+  programId: "program1",
+  category: Category.GENERAL,
+  gender: Gender.MALE,
+  rollNo: "230001001",
+};
+
 export const USER3: User = {
   id: "user3",
   name: "User 3",
   email: "user3@test.com",
   contact: "237842949",
   role: Role.RECRUITER,
+};
+
+export const RECRUITER1: Recruiter = {
+  id: "recruiter1",
+  companyId: COMPANY1.id,
+  userId: USER3.id,
 };
 
 export const USER4: User = {
@@ -64,10 +82,23 @@ export const USER4: User = {
   role: Role.FACULTY,
 };
 
+export const FACULTY1: Faculty = {
+  id: "faculty1",
+  department: "department1",
+  userId: USER4.id,
+};
+
 export const USER5: User = {
   id: "user5",
   name: "User 5",
   email: "user5@test.com",
   contact: "237842949",
   role: Role.TPC_MEMBER,
+};
+
+export const TPC_MEMBER1: TpcMember = {
+  id: "tpcMember1",
+  userId: USER5.id,
+  department: "department2",
+  role: TpcMemberRole.MANAGER,
 };
