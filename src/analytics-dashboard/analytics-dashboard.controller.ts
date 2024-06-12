ffa4199af045b7bc0e23008bc6ca77f04fa1ee17
AryanGKulkarni <95529889+AryanGKulkarni@users.noreply.github.com>
2024-06-13 00:05:18 +0530

    dto added

--- src/analytics-dashboard/analytics-dashboard.controller.ts
+++ src/analytics-dashboard/analytics-dashboard.controller.ts
@@ -1,9 +1,16 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UnauthorizedException } from "@nestjs/common";
 import { AnalyticsDashboardService } from "./analytics-dashboard.service";
 import { AuthGuard } from "@nestjs/passport";
-import { ApiTags, ApiBearerAuth } from "@nestjs/swagger";
+import { ApiTags, ApiBearerAuth, ApiResponse } from "@nestjs/swagger";
 import { User } from "src/decorators/User";
 import { IUser } from "src/auth/User";
+import {
+  CategoryWiseStatsDto,
+  CourseWiseStatsDto,
+  DepartmentWiseStatsDto,
+  GenderWiseStatsDto,
+  StatsDto,
+} from "./dto/get.dto";
 
 @Controller("analytics-dashboard")
 @UseGuards(AuthGuard("jwt"))
@@ -13,52 +20,47 @@ export class AnalyticsDashboardController {
   constructor(private readonly analyticsDashboardService: AnalyticsDashboardService) {}
 
   @Get("statsOverall/:seasonId")
+  @ApiResponse({ type: StatsDto })
   async getPlacementStatisticsOverall(@Param("seasonId") seasonId: string, @User() user: IUser) {
-    /*
-     * if (user.role !== "ADMIN") {
-     *   throw new UnauthorizedException("You are not authorized to access this resource");
-     * }
-     */
+    if (user.role !== "ADMIN") {
+      throw new UnauthorizedException("You are not authorized to access this resource");
+    }
 
     return this.analyticsDashboardService.getStatsOverall(seasonId);
   }
   @Get("statsCourseWise/:seasonId")
+  @ApiResponse({ type: CourseWiseStatsDto })
   async getPlacementStatisticsCourseWise(@Param("seasonId") seasonId: string, @User() user: IUser) {
-    /*
-     * if (user.role !== "ADMIN") {
-     *   throw new UnauthorizedException("You are not authorized to access this resource");
-     * }
-     */
+    if (user.role !== "ADMIN") {
+      throw new UnauthorizedException("You are not authorized to access this resource");
+    }
 
     return this.analyticsDashboardService.getStatsCourseWise(seasonId);
   }
   @Get("statsDepartmentWise/:seasonId")
+  @ApiResponse({ type: DepartmentWiseStatsDto })
   async getPlacementStatisticsDepartmentWise(@Param("seasonId") seasonId: string, @User() user: IUser) {
-    /*
-     * if (user.role !== "ADMIN") {
-     *   throw new UnauthorizedException("You are not authorized to access this resource");
-     * }
-     */
+    if (user.role !== "ADMIN") {
+      throw new UnauthorizedException("You are not authorized to access this resource");
+    }
 
     return this.analyticsDashboardService.getStatsDepartmentWise(seasonId);
   }
   @Get("statsCategoryWise/:seasonId")
+  @ApiResponse({ type: CategoryWiseStatsDto })
   async getPlacementStatisticsCategoryWise(@Param("seasonId") seasonId: string, @User() user: IUser) {
-    /*
-     * if (user.role !== "ADMIN") {
-     *   throw new UnauthorizedException("You are not authorized to access this resource");
-     * }
-     */
+    if (user.role !== "ADMIN") {
+      throw new UnauthorizedException("You are not authorized to access this resource");
+    }
 
     return this.analyticsDashboardService.getStatsCategoryWise(seasonId);
   }
   @Get("statsGenderWise/:seasonId")
+  @ApiResponse({ type: GenderWiseStatsDto })
   async getPlacementStatisticsGenderWise(@Param("seasonId") seasonId: string, @User() user: IUser) {
-    /*
-     * if (user.role !== "ADMIN") {
-     *   throw new UnauthorizedException("You are not authorized to access this resource");
-     * }
-     */
+    if (user.role !== "ADMIN") {
+      throw new UnauthorizedException("You are not authorized to access this resource");
+    }
 
     return this.analyticsDashboardService.getStatsGenderWise(seasonId);
   }
