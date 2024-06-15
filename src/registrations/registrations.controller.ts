afbd9aa672403f6876e57b4c268a8f10a2da25d8
Ishaan Mittal <ishaan@truefoundry.com>
2024-06-15 15:54:36 +0530

    deregister

--- src/registrations/registrations.controller.ts
+++ src/registrations/registrations.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Query } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Body, Controller, HttpException, HttpStatus, Patch, Query, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { RegistrationsService } from "./registrations.service";
-import { DeleteValues, GetValues, PostValues } from "src/decorators/controller";
+import { DeleteValues, GetValues, PatchValues, PostValues } from "src/decorators/controller";
 import { RegistrationsQueryDto } from "./dtos/query.dto";
 import { GetRegistrationsDto } from "./dtos/get.dto";
 import { createArrayPipe, pipeTransformArray } from "src/utils/utils";
 import { CreateRegistrationsDto } from "./dtos/post.dto";
 import { DeleteValuesDto } from "src/utils/utils.dto";
+import { AuthGuard } from "@nestjs/passport";
+import { IUser } from "src/auth/User";
+import { User } from "src/decorators/User";
+import { RoleEnum } from "src/enums";
 
 @Controller("registrations")
 @ApiTags("Registration")
+@ApiBearerAuth("jwt")
+@UseGuards(AuthGuard("jwt"))
 export class RegistrationsController {
   constructor(private registrationsService: RegistrationsService) {}
 
@@ -22,15 +28,29 @@ export class RegistrationsController {
 
   @PostValues(CreateRegistrationsDto)
   async createRegistrations(@Body(createArrayPipe(CreateRegistrationsDto)) registrations: CreateRegistrationsDto[]) {
-    const ans = await this.registrationsService.createRegistrations(registrations);
-
-    return ans;
+    return await this.registrationsService.createRegistrations(registrations);
   }
 
   @DeleteValues()
   async deleteRegistrations(@Query() query: DeleteValuesDto) {
-    const ans = await this.registrationsService.deleteRegistrations(query.id);
+    return await this.registrationsService.deleteRegistrations(query.id);
+  }
 
-    return ans;
+  @PatchValues(CreateRegistrationsDto)
+  async updateRegistrations(
+    @Body(createArrayPipe(CreateRegistrationsDto)) registrations: CreateRegistrationsDto[]
+  ): Promise<string[]> {
+    return await this.registrationsService.createRegistrations(registrations);
+  }
+
+  @Patch("/de-register")
+  async deRegister(@Body() registraion: CreateRegistrationsDto, @User() user: IUser): Promise<string[]> {
+    if (
+      !(user.role === RoleEnum.ADMIN || (user.role === RoleEnum.STUDENT && user.studentId == registraion.studentId))
+    ) {
+      throw new HttpException("Unauthorized", HttpStatus.UNAUTHORIZED);
+    }
+
+    return await this.registrationsService.createRegistrations([{ ...registraion, registered: false }]);
   }
 }
