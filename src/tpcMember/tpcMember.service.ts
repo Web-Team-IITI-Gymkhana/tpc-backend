c1d106198b44df39ddae88dd5981aae9c2638ca6
princekumarofficial <x.prince.x@outlook.com>
2025-05-22 16:50:40 +0530

    Bug: Add role assignment for newly created TPC members

--- src/tpcMember/tpcMember.service.ts
+++ src/tpcMember/tpcMember.service.ts
@@ -13,6 +13,7 @@ import {
   TpcMemberModel,
   UserModel,
 } from "src/db/models";
+import { RoleEnum } from "src/enums/role.enum"; // Import RoleEnum
 import { TpcMembersQueryDto } from "./dtos/query.dto";
 import { FindOptions } from "sequelize";
 import { parseFilter, parseOrder, parsePagesize } from "src/utils";
@@ -109,6 +110,23 @@ export class TpcMemberService {
 
   async createTpcMembers(tpcMembers: CreateTpcMembersDto[]) {
     const ans = await this.tpcMemberRepo.bulkCreate(tpcMembers);
+
+    // Update the user role to TPC_MEMBER for the created TPC members
+    const studentIds = ans.map((tpcMember) => tpcMember.studentId);
+    const userIds = await StudentModel.findAll({
+      where: {
+        id: studentIds,
+      },
+      attributes: ["userId"],
+    }).then((students) => students.map((student) => student.userId));
+    await UserModel.update(
+      { role: RoleEnum.TPC_MEMBER },
+      {
+        where: {
+          id: userIds,
+        },
+      }
+    );
 
     return ans.map((tpcMember) => tpcMember.id);
   }
