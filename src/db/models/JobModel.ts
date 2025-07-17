b37d6d3e877eab05cf127a4385be41cc1a715240
Prince Kumar <x.prince.x@outlook.com>
2025-07-17 14:16:22 +0530

    Revert "Develop"

--- src/db/models/JobModel.ts
+++ src/db/models/JobModel.ts
@@ -266,7 +266,7 @@ export class JobModel extends Model<JobModel> {
 
     const adminPath = path.resolve(process.cwd(), "src/html", "JafToAdmin.html");
     const adminReplacements = {
-      companyName: instance.company.name,
+      companyName: (instance.companyDetailsFilled as ICompanyDetails).name,
       url: url,
     };
     const adminContent = getHtmlContent(adminPath, adminReplacements);
@@ -277,7 +277,7 @@ export class JobModel extends Model<JobModel> {
       from: { name: APP_NAME, address: MAIL_USER },
       recepients: [...emails],
       // recepients: [{ address: DEFAULT_MAIL_TO }],
-      subject: `Job Announcement Form Filled by ${instance.company.name}`,
+      subject: `Job Announcement Form Filled by ${(instance.companyDetailsFilled as ICompanyDetails).name}`,
       html: adminContent,
     };
 
